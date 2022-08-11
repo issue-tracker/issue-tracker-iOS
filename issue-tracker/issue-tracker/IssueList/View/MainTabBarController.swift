937e90dbb0be265d8e0db36f604f377c2d976304
백상휘 <sanghwiback@gmail.com>
2022-08-11 20:07:20 +0900

    Delete unnecessary comments

--- issue-tracker/issue-tracker/IssueList/View/MainTabBarController.swift
+++ issue-tracker/issue-tracker/IssueList/View/MainTabBarController.swift
@@ -8,27 +8,7 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-//
-//        let issueList = IssueListViewController()
-//        let myPage = MyPageViewController()
-//        let setting = SettingsViewController()
-//        
-//        let issueListNav = IssueListNavigationController(rootViewController: issueList)
-//        let myPageNav = MyPageNavigationController(rootViewController: myPage)
-//        let settingNav = SettingsNavigationController(rootViewController: setting)
-//        
-//        issueListNav.tabBarItem.title = "IssueList"
-//        issueListNav.tabBarItem.image = UIImage(systemName: "list.dash")
-//        
-//        myPageNav.tabBarItem.title = "MyPage"
-//        myPageNav.tabBarItem.image = UIImage(systemName: "person.fill.questionmark")
-//        
-//        settingNav.tabBarItem.title = "Setting"
-//        settingNav.tabBarItem.image = UIImage(systemName: "gear")
-//        
-//        viewControllers = [issueListNav, myPageNav, settingNav]
     }
 }

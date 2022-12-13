87fb83e596bc4acfc52d826935f8cb8dd11c97f7
백상휘 <sanghwiback@gmail.com>
2022-12-13 17:22:07 +0900

    Fix error ko.lproj
    - 오타로 인한 테스트 실패.

--- issue-tracker/issue-trackerUnitTest/issue_trackerUnitTest_Localizable.swift
+++ issue-tracker/issue-trackerUnitTest/issue_trackerUnitTest_Localizable.swift
@@ -10,7 +10,11 @@ import XCTest
 final class issue_trackerUnitTest_Localizable: XCTestCase {
     func testLocalizableLogin() throws {
       XCTAssertEqual(I18N.L_N_LVC_TXTF_ID, NSLocalizedString("L_N_LVC_TXTF_ID", comment: ""))
-        
-        print(I18N.L_N_LVC_TXTF_ID)
+      XCTAssertEqual(I18N.L_N_LVC_TXTF_PW, NSLocalizedString("L_N_LVC_TXTF_PW", comment: ""))
+      XCTAssertEqual(I18N.L_N_LVC_BTN_LOGIN, NSLocalizedString("L_N_LVC_BTN_LOGIN", comment: ""))
+      XCTAssertEqual(I18N.L_N_LVC_BTN_SIGNIN, NSLocalizedString("L_N_LVC_BTN_SIGNIN", comment: ""))
+      XCTAssertEqual(I18N.L_N_LVC_BTN_PWRESET, NSLocalizedString("L_N_LVC_BTN_PWRESET", comment: ""))
+      XCTAssertEqual(I18N.L_N_LVC_BTN_SIGNIN_OAUTH, NSLocalizedString("L_N_LVC_BTN_SIGNIN_OAUTH", comment: ""))
+      XCTAssertEqual(I18N.L_N_LVC_LB_SIGN_OAUTH, NSLocalizedString("L_N_LVC_LB_SIGN_OAUTH", comment: ""))
     }
 }

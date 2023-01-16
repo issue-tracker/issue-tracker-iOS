a3c6fcdaa717c90169615675257b3b4e3630d1ce
백상휘 <sanghwiback@gmail.com>
2023-01-16 14:25:21 +0900

    Add FirebaseStorage for Universal Link

--- issue-tracker/issue-tracker/Resources/AppDelegate.swift
+++ issue-tracker/issue-tracker/Resources/AppDelegate.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
         
 //        UserDefaults.standard.setValue(
 //            (try? JSONEncoder().encode([
